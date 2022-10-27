b6839e437dd6e2523f9026d6ad6b0274f4c8049f
Maryam Abdulle <iammaryamabdulle@gmail.com>
2022-10-27 14:34:09 -0700

    Almost done
    Co-authored-by: Maryam Abdulle <imamaryamabdulle@gmail.com>
    Co-authored-by: leyasalazar  <96083374+leyasalazar@users.noreply.github.com>

---
+++ crud.py
@@ -0,0 +1,43 @@
+"""CRUD operations."""
+
+from model import db, User, Movie, Rating, connect_to_db
+
+
+# Functions start here!
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    return user
+
+def get_users():
+    """Return the user info."""
+    return User.query.all()
+def get_user_by_email(email):
+    return User.query.filter_by(email =email). first()
+
+
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie."""
+    movie = Movie(title=title, overview=overview, release_date=release_date, poster_path=poster_path)
+
+    return movie
+
+def all_movies():
+    """Return all movies."""
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    return Movie.query.get(movie_id)
+
+
+def create_rating(user, movie, score):
+    """Create and return a new rating."""
+    rating= Rating(user=user, movie=movie, score=score)
+    return rating
+    
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
